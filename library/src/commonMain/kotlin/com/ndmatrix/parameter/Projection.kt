e0c9bca02ca9aa4a9f88d7ce6146277b9ba0b933
alexskakun <skakun@peterpartner.net>
2025-03-28 14:52:11 +0300

    8

--- library/src/commonMain/kotlin/com/ndmatrix/parameter/Projection.kt
+++ library/src/commonMain/kotlin/com/ndmatrix/parameter/Projection.kt
@@ -1,7 +1,3 @@
 package com.ndmatrix.parameter
 
-import kotlin.coroutines.CoroutineContext
-
-abstract class Projection<S: Any>: Parameter<S> {
-
-}
+abstract class Projection<S: Any>: Parameter<S>
